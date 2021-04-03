8841f8b45be5039ed72c16732543f5f82958ecad
Andrew Bridge <andrew@andrewhbridge.co.uk>
2021-04-03 13:46:05 +0100

    Add the hyper-apps file to the repo

---
+++ hammerspoon/hyper-apps.lua
@@ -0,0 +1,12 @@
+-- Default keybindings for launching apps in Hyper Mode
+--
+-- To launch _your_ most commonly-used apps via Hyper Mode, create a copy of
+-- this file, save it as `hyper-apps.lua`, and edit the table below to configure
+-- your preferred shortcuts.
+return {
+  { 'a', 'Activity Monitor' },  -- "A" for "Activity Monitor"
+  { 'c', 'Visual Studio Code'},       -- "C" for "Code"
+  { 'b', 'Firefox Developer Edition' }, -- "B" for "Browser"
+  { 'f', 'Finder' },            -- "F" for "Finder"
+  { 't', 'iTerm' },             -- "T" for "Terminal"
+}
